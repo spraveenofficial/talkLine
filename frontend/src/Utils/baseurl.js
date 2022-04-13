529833a178d836024659b31e8dacef613daf3458
Praveen <spraveen593@gmail.com>
2022-04-13 06:42:49 +0530

    Changed BaseUrl after Deployment

--- frontend/src/Utils/baseurl.js
+++ frontend/src/Utils/baseurl.js
@@ -1,4 +1,6 @@
 const baseUrl =
-  process.env.REACT_APP_MODE === "dev" ? "http://localhost:4000/v1/api" : null;
+  process.env.REACT_APP_MODE === "dev"
+    ? "http://localhost:4000/v1/api"
+    : "https://talk-line-backend.vercel.app/v1/api";
 
 export default baseUrl;
