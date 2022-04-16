9cb175ddb61f58e6b60a0c3286e9b7353f7b83ee
Praveen <spraveen593@gmail.com>
2022-04-16 21:43:07 +0530

    Added Profile Reducer and Constant

--- frontend/src/Redux/Reducers/index.js
+++ frontend/src/Redux/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { auth, signup, verifyOtp, uploadAvatar, login } from "./auth-reducers";
+import { profile } from "./profile-reducers";
 export default combineReducers({
   auth,
   signup,
   verifyOtp,
   uploadAvatar,
   login,
+  profile,
 });
---
+++ frontend/src/Redux/Reducers/profile-reducers.js
@@ -0,0 +1,40 @@
+import {
+  FETCH_PROFILE_REQUEST,
+  FETCH_PROFILE_SUCCESS,
+  FETCH_PROFILE_FAILURE,
+  CLEAR_PROFILE,
+} from "../Constants/profile-constants";
+export const profile = (
+  state = { loading: false, success: false, user: {}, message: "" },
+  action
+) => {
+  switch (action.type) {
+    case FETCH_PROFILE_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_PROFILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        success: true,
+        user: action.payload,
+      };
+    case FETCH_PROFILE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        success: false,
+        user: {},
+        message: action.payload,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        loading: false,
+        success: false,
+        user: {},
+        message: "",
+      };
+    default:
+      return state;
+  }
+};
