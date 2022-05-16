e13a2ebda84c303bb27e2efccef6ea98f9d44de9
Praveen <spraveen593@gmail.com>
2022-05-16 17:52:46 +0530

    Fix: Chat Page width

--- frontend/src/Pages/Messages/index.jsx
+++ frontend/src/Pages/Messages/index.jsx
@@ -56,7 +56,7 @@ export function Message() {
   };
   const handleType = useCallback(debounce(handleSearchFriends), []);
   return (
-    <div className="w-2/3 bg-white block p-2 mobile:w-full mobile:py-0">
+    <div className="fix-chat-width bg-white p-2 mobile:w-full mobile:py-0">
       <div className="w-full p-4 bg-indigo-100 flex text-center items-center gap-2 rounded-2xl">
         <h2 className="text-2xl font-bold">Messaging</h2>
         <MessageIcon className="h-8 w-8" />
